1cca7fbe5059f8267ed167ff68e076bedea403c3
SufyanTariq123 <SufyanTariq056@gmail.com>
2020-06-09 01:19:50 +0500

    Updates

---
+++ precache-manifest.414dd951ce3ca776b83d19b7c1d29589.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07d4c8f8357a40c6b7bdd953af2965d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "87d3887b0647040f7c18",
+    "url": "/portfolio/static/css/main.305ece6b.chunk.css"
+  },
+  {
+    "revision": "4ab9a8db08c2f694db17",
+    "url": "/portfolio/static/js/2.97b0fc3a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.97b0fc3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d3887b0647040f7c18",
+    "url": "/portfolio/static/js/main.5ad9085e.chunk.js"
+  },
+  {
+    "revision": "32d639bc7db5432d2ec9",
+    "url": "/portfolio/static/js/runtime-main.612b018b.js"
+  },
+  {
+    "revision": "ebd60090a5837eb99919360a113b312e",
+    "url": "/portfolio/static/media/college.ebd60090.png"
+  },
+  {
+    "revision": "ddcc25e4e23d2204c565d542341dde57",
+    "url": "/portfolio/static/media/covid.ddcc25e4.jpg"
+  },
+  {
+    "revision": "acbad608bcf83d3adaccaae949e22997",
+    "url": "/portfolio/static/media/food.acbad608.jpeg"
+  },
+  {
+    "revision": "59cb21f7211518e42313d1f2c9e52969",
+    "url": "/portfolio/static/media/university.59cb21f7.png"
+  },
+  {
+    "revision": "20f5f8b13a03a447f18fd4329c3f320a",
+    "url": "/portfolio/static/media/w.20f5f8b1.jpg"
+  }
+]);
